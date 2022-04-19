bf14367bb527f6ebada46ac20ccec08ac02befef
Jack Rhys-Burgess <jackrhysburgess@googlemail.com>
2022-04-19 17:54:44 +0100

    Add a lot: fix csvs, add barebones structure to plot every level using (wr index, relative improvement) axes, polyfit curve fitting

--- csvs/Readme.txt
+++ csvs/Readme.txt
@@ -3,4 +3,6 @@ https://docs.google.com/spreadsheets/d/1Dj1HEUXkyALK8qCAuzAEetnvJhvJjWBxd1Nr5GR5
 
 This is here for posterity if needed
 
-Although on re-re-flection, any time missing 3rd decimal with 9, if needed, cuz that's what we do innit bruv
+Only processing involved are removing `unknown` times (i.e. times not known to 2dp) and appending 9 to times where the 3rd decimal is unknown/not given
+(and converting times over a minute to purely seconds, which aren't common but some of the very very early times were)
+One time on Siege was corrected as it went 18.xxx > 28.xxx > 18.xxx ??
--- main.py
+++ main.py
@@ -43,6 +43,92 @@ from scipy import interpolate
 def main():
     print("its bare here now!!")
 
+    #relative_improvement_testfunc()
+
+    #Define needed variables up here for scoping/keeping-track reasons:
+    times = []
+    rel_improv = []
+    #big 'ol list of all level names, for graph title purposes
+    level_names = ["Learning to Roll", "Collect the Gems", "Jump Training", "Learn the Super Jump", "Platform Training", "Learn the Super Speed", "Elevator", "Air Movement", "Gyrocopter", "Time Trial", "Super Bounce", "Gravity Helix", "Shock Absorber", "There and Back Again", "Marble Materials Lab", "Bumper Training", "Breezeway", "Mine Field", "Trapdoors!", "Tornado Bowl", "Pitfalls", "Platform Party", "Winding Road", "Grand Finale",
+    "Jump Jump Jump", "Monster Speedway Qualifying", "Skate Park", "Ramp Matrix", "Hoops", "Go for the Green", "Fork in the Road", "Tri Twist", "Marbletris", "Space Slide", "Skee Ball Bonus", "Marble Playground", "Hop Skip and a Jump", "Take the High Road", "Half-Pipe", "Gauntlet", "Moto-Marblecross", "Shock Drop", "Spork in the Road", "Great Divide", "The Wave", "Tornado Alley", "Monster Speedway", "Upward Spiral",
+    "Thrill Ride", "Money Tree", "Fan Lift", "Leap of Faith", "Freeway Crossing", "Stepping Stones", "Obstacle Course", "Points of the Compass", "Three-Fold Maze", "Tube Treasure", "Slip 'n Slide", "Skyscraper", "Half Pipe Elite", "A-Maze-ing", "Block Party", "Trapdoor Madness", "Moebius Strip", "Great Divide Revisted", "Escher's Race", "To the Moon", "Around the World in 30 seconds", "Will o' the Wisp", "Twisting the night away", "Survival of the Fittest", 
+    "Plumber's Portal", "Ski Slopes", "Ramps Reloaded", "Tower Maze", "Free Fall", "Acrobat", "Whirl", "Mudslide", "Pipe Dreams", "Scaffold", "Airwalk", "Shimmy", "Path of Least Resistance", "Daedalus", "Ordeal", "Battlements", "Pinball Wizard", "Eye of the Storm", "Dive!", "Tightrope", "Natural Selection", "Tango", "Icarus", "Under Construction", "Pathways", "Darwin's Dilemna", "King of the Mountain"
+    ]
+
+    #CSV reading time
+    for num in range(1,2): #purely for testing purposes
+    #for num in range(1,101): #range func is (inclusive, exclusive)
+        print("---BEGIN CSV TIME FETCHING---")
+        with open(f"csvs/{num}.csv", newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            #date_test = [] #Only for testing purposes, ensure csv is consistent
+            for row in reader:
+                times.append(row['Time'])
+                #date_test.append(row['Date']) #Only for testing purposes
+
+            #Convert items in list from strings to floats
+            #Only convert to numpy array when needed!
+            times = [float(i) for i in times]
+
+        ##END getting times from csv
+        print("---END CSV TIME FETCHING----")
+        ##BEGIN creating rel_improv array for graphing
+        print("---BEGIN RELATIVE IMPROVEMENT CALCULATION---")
+
+        for num1 in range(0, len(times)):
+            rel_change = (times[0] - times[num1])/(times[num1])
+            rel_improv.append(rel_change)
+        
+        print(rel_improv)
+
+        print("---END RELATIVE IMPROVEMENT CALCULATION---")
+        print("---BEGIN CREATING PLOTS---")
+        rel_improv = np.array(rel_improv)
+        wr_index = np.arange(0, len(rel_improv))
+        level_name = level_names[num - 1]
+        plt.scatter(wr_index, rel_improv)
+
+        #Let's do some lines/curves of best fit yayayay
+        #polyfit line of best fit
+        a, b = np.polyfit(wr_index, rel_improv, 1)
+        y_linefit = a*wr_index + b
+        plt.plot(wr_index, y_linefit, label="1st deg")
+
+        #polyfit 2nd degree curve of best fit
+        a, b, c = np.polyfit(wr_index, rel_improv, 2)
+        y_2ndcurve = a*pow(wr_index, 2) + b*wr_index + c
+        plt.plot(wr_index, y_2ndcurve, label="2nd deg")
+
+        #polyfit 3rd degree curve
+        a, b, c, d = np.polyfit(wr_index, rel_improv, 3)
+        y_3rdcurve = a*pow(wr_index, 3) + b*pow(wr_index, 2) + c*wr_index + d
+        plt.plot(wr_index, y_3rdcurve, label="3rd deg")
+
+        #polyfit 4th degree curve
+        a, b, c, d, e = np.polyfit(wr_index, rel_improv, 4)
+        y_4thcurve = a*pow(wr_index, 4) + b*pow(wr_index, 3) + c*pow(wr_index, 2) + d*wr_index + e
+        plt.plot(wr_index, y_4thcurve, label="4th deg")
+
+        plt.legend(title = "Using np.polyfit")
+        plt.xlabel("WR Index")
+        plt.ylabel("Relative Improvement")
+        plt.title(f"WR History of {level_name}")
+        plt.savefig("figs/test.png")
+        plt.show()
+
+def relative_improvement_testfunc():
+    print("test")
+    times = [3.999,3.89,3.266,3.074,3.069,3.049,2.985,2.984,2.402,2.37,2.333,2.287,2.281,2.267,2.25,2.236,2.226]
+    rel_improv = []
+
+    for num in range(0, len(times)):
+        rel_change = (times[0] - times[num])/(times[num])
+        rel_improv.append(rel_change)
+        
+    print(len(times))
+    print(len(rel_improv))
+    print(rel_improv)
+
 
 if __name__ == "__main__":
     main()
